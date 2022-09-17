3ff2c4e09da71bd7ee6b0d60850e753f8bc8ddb6
HansBug <hansbug@buaa.edu.cn>
2022-09-17 04:40:15 +0000

    fix(hansbug): fix the not-runnable problems

--- template/main.py
+++ template/main.py
@@ -1,9 +1,10 @@
 import os
 import subprocess
 from tqdm import tqdm
+import sys
 
 if __name__ == "__main__":
     experiment_list = [os.path.dirname(t['dst']) for t in {{user.task_path}}]
     for path in tqdm(experiment_list, desc="DI-engine experiment with {} x {}".format({{user.env}}, {{user.algo}})):
         with open(os.path.join(path, 'output.txt'), "w") as outfile:
-            subprocess.run("cd {} && python3 -u main.py".format(path), shell=True, stdout=outfile, stderr=outfile)
+            subprocess.run(f"cd {path!r} && {sys.executable!r} -u main.py", shell=True, stdout=outfile, stderr=outfile)
