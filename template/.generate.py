48c5eb49e1c20add78529982809dfd5a3244215c
HansBug <hansbug@buaa.edu.cn>
2022-09-17 13:05:05 +0800

    dev(hansbug): refactor this template

--- template/.generate.py
+++ template/.generate.py
@@ -1,10 +1,17 @@
 import os
 import shutil
-from igm.render import igm_script
-from igm.env import user
-from extras import project_dir
 
-multi_seed_string = "import os\n\n" + "if __name__ == \"__main__\":\n  for s in placeholder:\n    os.popen('python3 -u main_seed{}.py'.format(s))"
+from extras import project_dir
+from igm.env import user
+from igm.render import igm_script
+
+_MULTI_SEED_CODE = """
+import os
+
+if __name__ == "__main__":
+    for s in placeholder:
+        os.popen('python3 -u main_seed{}.py'.format(s))
+"""
 
 
 @igm_script
@@ -23,7 +30,7 @@ def generate_experiment():
                     os.popen("sed -i '' 's/seed=0/seed={}/' {}".format(s, dst_s))
                     os.popen("sed -i '' 's/seed0/seed{}/' {}".format(s, dst_s))
                 with open(dst, 'w') as f:
-                    f.write(multi_seed_string)
+                    f.write(_MULTI_SEED_CODE)
                 os.popen("sed -i '' 's/placeholder/{}/' {}".format(user.seed, dst))
             else:
                 os.popen("sed -i '' 's/seed=0/seed={}/' {}".format(user.seed[0], dst))
--- template/main.py
+++ template/main.py
@@ -1,10 +1,13 @@
 import os
 import subprocess
-from tqdm import tqdm
 import sys
 
+from tqdm import tqdm
+
 if __name__ == "__main__":
-    experiment_list = [os.path.dirname(t['dst']) for t in {{user.task_path}}]
-    for path in tqdm(experiment_list, desc="DI-engine experiment with {} x {}".format({{user.env}}, {{user.algo}})):
+    experiment_list = [os.path.dirname(t['dst']) for t in {{user.task_path | potc}}]
+    for path in tqdm(experiment_list,
+                     desc="DI-engine experiment with {} x {}".format({{user.env | potc}}, {{user.algo | potc}})):
         with open(os.path.join(path, 'output.txt'), "w") as outfile:
-            subprocess.run(f"cd {path!r} && {sys.executable!r} -u main.py", shell=True, stdout=outfile, stderr=outfile)
+            subprocess.run(f"cd {path!r} && {sys.executable!r} -u main.py",
+                           shell=True, stdout=outfile, stderr=outfile)
